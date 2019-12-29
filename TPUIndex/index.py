0d1a21bd817c63586cba3ef419fb38af78867505
srihari-humbarwadi <sriharihumbarwadi97@gmail.com>
2019-12-29 19:47:08 +0530

    added demo notebook

--- TPUIndex/index.py
+++ TPUIndex/index.py
@@ -5,6 +5,7 @@ try:
     tpu = tf.distribute.cluster_resolver.TPUClusterResolver()
     tf.config.experimental_connect_to_cluster(tpu)
     tf.tpu.experimental.initialize_tpu_system(tpu)
+    strategy = tf.distribute.experimental.TPUStrategy(tpu)
 except ValueError:
     print('Failed initializing TPU, cannot proceed')
 
